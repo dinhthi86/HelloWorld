d5fc1409c6f6b3b149eeb4fa57e871c49ce6c59f
Thi Vu <dinhthi86@gmail.com>
2021-04-18 23:01:44 +0900

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[]args) {
-        System.out.println("Welcome to Jankins!");
+        System.out.println("Welcome to Jenkins with Github!");
     }
 }
